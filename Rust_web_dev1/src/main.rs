399aad02650a48c9af7365e9f3dc86b1dba27eb7
Niroshan-K <lakshanniroshan822@gmail.com>
2026-09-20 22:08:56 +0530

    created first axum api

--- Rust_web_dev1/src/main.rs
+++ Rust_web_dev1/src/main.rs
@@ -1,33 +1,13 @@
-use axum::{Json, Router, extract::{Path, Query}, http::{StatusCode}, routing::get};
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Clone)]
-struct Task {
-    id: u32,
-    title: String,
-    done: bool
-}
-
-#[derive(Deserialize)]
-struct CreateTask {
-    title: String
-}
-
-fn seed_task() -> Vec<Task> {
-    vec![
-        Task { id: 1, title: "Learn Axum".to_string(), done: false},
-        Task { id: 2, title: "Write a script".to_string(), done: false},
-        Task { id: 3, title: "Record a video".to_string(), done: false},
-    ]
-}
+use crate::state::AppState;
+mod models;
+mod state;
+mod routes;
+mod handlers;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/health", get(health))
-        .route("/task", get(list_tasks).post(create_task))
-        .route("/tasks/{id}", get(get_task));
+    let state = AppState::new();
+    let app = routes::app(state);
 
     let listener = tokio::net::TcpListener::bind
         ("0.0.0.0:3000").await.unwrap();
@@ -35,36 +15,4 @@ async fn main() {
     println!("task running on http://localhost:3000");
 
     axum::serve(listener, app).await.unwrap();
-}
-
-async fn root() -> &'static str {
-    "task is alive"
-}
-
-async fn health() -> &'static str {
-    "status: 200"
-}
-
-async fn list_tasks() -> Json<Vec<Task>> {
-    Json(seed_task())
-}
-
-async fn create_task(Json(payload): Json<CreateTask>) -> (StatusCode, Json<Task>) {
-    let task = Task {
-        id: 99,
-        title: payload.title,
-        done: false
-    };
-
-    (StatusCode::CREATED, Json(task))
-}
-
-async fn get_task(Path(id): Path<u32>) -> Result<Json<Task>, StatusCode> {
-    let tasks = seed_task();
-
-    tasks
-        .into_iter()
-        .find(|t| t.id == id)
-        .map(Json)
-        .ok_or(StatusCode::NOT_FOUND)
 }
---
+++ Rust_web_dev1/src/state.rs
@@ -0,0 +1,23 @@
+use crate::models::Task;
+use std::sync::{Arc, Mutex};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub tasks: Arc<Mutex<Vec<Task>>>,
+    pub next_id: Arc<Mutex<u32>>
+}
+
+impl AppState {
+    pub fn new() -> Self {
+        let tasks = vec![
+            Task { id: 1, title: "Learn Axum".to_string(), done: false},
+            Task { id: 2, title: "Write a script".to_string(), done: false},
+            Task { id: 3, title: "Record a video".to_string(), done: false},
+        ];
+
+        Self{
+            tasks: Arc::new(Mutex::new(tasks)),
+            next_id: Arc::new(Mutex::new(4))
+        }
+    }
+}
